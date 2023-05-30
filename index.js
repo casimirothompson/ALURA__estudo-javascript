67951e618ad7a30ff97d7ef3a7c1ac2b27372dd7
casimiro thompson <casimirothompson@gmail.com>
2023-05-30 00:53:15 -0300

    adicionando mais 3 operações

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const primeiroNum = 1;
 const segundoNum = 2;
 
 const operacaoMatematica = primeiroNum + segundoNum;
+const operacaoMatematica2 = primeiroNum - segundoNum;
+const operacaoMatematica3 = primeiroNum * segundoNum;
+const operacaoMatematica4 = primeiroNum / segundoNum;
 
 console.log(operacaoMatematica);
