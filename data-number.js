2b89ef0552d75c08343649da89b1d09b6748ccc5
casimiro thompson <casimirothompson@gmail.com>
2023-05-30 01:25:43 -0300

    finalizado data-number e adcionando novos arquivos

---
+++ data-number.js
@@ -0,0 +1,30 @@
+const firstNum = 3;
+const secondNum = 5;
+
+let soma, sub, multi, div;
+soma = firstNum + secondNum;
+sub = firstNum - secondNum;
+multi = firstNum * secondNum;
+div = firstNum / secondNum;
+
+console.log(soma);
+console.log(sub);
+console.log(multi);
+console.log(div);
+
+//NUMEROS FLUTUANTES PODEM SER REPRESENTADOS COM numero1.numero2 => 2.2, 3.5, 1.7;
+//e tambem somente com .numero => .1, .2, .75 [porem sempre antes do . é o 0];
+
+const primeiNum = 1.9;
+const segundNum = 2.8;
+
+let soma1, sub2, multi3, div4;
+soma1 = primeiNum + segundNum;
+sub2 = primeiNum - segundNum;
+multi3 = primeiNum * segundNum;
+div4 = primeiNum / segundNum;
+
+console.log(soma1)
+console.log(sub2)
+console.log(multi3)
+console.log(div4)
