e742c8a9926005afb354490d6d4545dcefa8653d
casimiro thompson <casimirothompson@gmail.com>
2023-05-30 00:54:53 -0300

    adicionado novas operações

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ const operacaoMatematica3 = primeiroNum * segundoNum;
 const operacaoMatematica4 = primeiroNum / segundoNum;
 
 console.log(operacaoMatematica);
+console.log(operacaoMatematica2);
+console.log(operacaoMatematica3);
+console.log(operacaoMatematica4);
